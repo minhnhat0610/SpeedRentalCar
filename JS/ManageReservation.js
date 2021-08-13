4a45f79a14f2f269562934fa051b1d5b3b33c1df
Your Name <you@example.com>
2021-08-12 22:45:30 -0400

    Finalize front end

--- JS/ManageReservation.js
+++ JS/ManageReservation.js
@@ -7,6 +7,34 @@ $(document).ready(function(){
         // display loader after click if valid input
         if(isValid){
             $('.loader').addClass('loading');
+
+
+            // wait until content is loaded in background before reveal it
+            RevealLayout();
         }
     })
+
+    let RevealLayout= () => {
+        //Change layout for Laptop
+        $('.body-inner-container').removeClass('first-loading');
+        $('.form-container').removeClass('active-form');
+
+        //Change layout for mobile
+        let CustomerContainerOffset = $('.customer-infor-container').offset().top;
+            
+            $('.body-content').css({
+                height: 'auto'
+            })
+            $('html, body').animate({
+                scrollTop: CustomerContainerOffset
+            },500)
+
+
+        //reveal content for both laptop and mobile
+        $('.customer-infor-container').removeClass('hide-customer-container');
+        $('.vehicle-infor-container').removeClass('hide-vehicle-container');
+
+        
+    }
+
 })
